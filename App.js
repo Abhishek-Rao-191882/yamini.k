c77407fe6c1e632a6bc6d2d532ef7ab36ea11f39
Yamini2001 <60056299+Yamini2001@users.noreply.github.com>
2021-08-12 21:23:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchArea from "./SearchArea";
+
+const App = () => {
+  return( 
+    <div>
+      <header>
+        <a href="/">Wetube</a>
+      
+      </header>
+       <SearchArea></SearchArea>
+      
+    </div>
+  )
+  
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
